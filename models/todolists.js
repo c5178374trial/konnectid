9dedcfe3a2e5e19732e7b8893f6b15a2f6186d62
iharsivak <ihar.sivak@leverx.com>
2016-03-10 14:50:05 +0300

    Some cleanup.

--- models/todolists.js
+++ models/todolists.js
@@ -1,26 +1,10 @@
 var mongoose = require('mongoose');
 
-//var TodoItem = require('./todos');
-
 // Simple schema that represents list of todos
 var todoListSchema = mongoose.Schema({
   title: String,
   description: String
 });
-
-//// helper method that returns all related todos
-//todoListSchema.virtual('items').get(function (cb) {
-//  return TodoItem.find({list: this._id}).exec(cb);
-//});
-
-todoListSchema.methods.createItem = function (title, description, author, cb) {
-  return new TodoItem({
-    'title': title,
-    'description': description,
-    'author': author,
-    'list': this._id
-  }).save(cb);
-};
 
 // return a model based upon the defined schema
 module.exports = mongoose.model('TodoList', todoListSchema);
--- models/todos.js
+++ models/todos.js
@@ -1,14 +1,11 @@
 var mongoose = require('mongoose');
 
-// Create a new schema for our tweet data
+// Create a new schema for our todo record
 var todoItemSchema = new mongoose.Schema({
   lists: [String],
   title: String,
   description: String,
   state: {type: Boolean, default: false}
-}, {
-  toObject: {virtuals: true},
-  toJSON: {virtuals: true}
 });
 
 todoItemSchema.methods.toggle = function () {
